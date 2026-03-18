9fd430cef6ca2e1158c107c390ecd47019c8e3b0
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 20:43:59 -0300

    refactor: Improve type hinting and formatting in Hit class

--- src/hit.py
+++ src/hit.py
@@ -1,9 +1,11 @@
 import glm
-
+from typing import Optional
+from phong_material import PhongMaterial
+from point_light import PointLight
 class Hit:
   def __init__(self, t=float('inf')):
-      self.t = t
-      self.pos = glm.vec3(0)
-      self.normal = glm.vec3(0)
-      self.material = None
-      self.light = None
+    self.t = t
+    self.pos = glm.vec3(0)
+    self.normal = glm.vec3(0)
+    self.material: Optional[PhongMaterial] = None
+    self.light: Optional[PointLight] = None
