30a133e3338770a8b91ca4526a4899da4d5f8b98
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 20:38:01 -0300

    feat: Add Camera class for ray generation in 3D space

---
+++ src/camera.py
@@ -0,0 +1,17 @@
+import glm
+from ray import Ray
+
+class Camera:
+  def __init__(self, eye, center, up, fov, width, height):
+    self.eye = glm.vec3(eye)
+    self.inv_view = glm.inverse(glm.lookAt(self.eye, glm.vec3(center), glm.vec3(up)))
+    self.aspect = width / height
+    self.fov_tan = glm.tan(glm.radians(fov) / 2.0)
+
+  def generate_ray(self, xn, yn):
+    # [cite: 4, 29]
+    dv = self.fov_tan
+    du = dv * self.aspect
+    p_cam = glm.vec4(-du + 2.0 * du * xn, dv - 2.0 * dv * yn, -1.0, 1.0)
+    p_world = self.inv_view * p_cam
+    return Ray(self.eye, glm.vec3(p_world) - self.eye)
