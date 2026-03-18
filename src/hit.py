0392d1957b1d6b773deda3dbfb6093cecbc27656
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 20:33:46 -0300

    feat: Add Hit class for ray intersection handling

---
+++ src/hit.py
@@ -0,0 +1,10 @@
+
+from pyglm import glm
+
+class Hit:
+  def __init__(self, t=float('inf')):
+      self.t = t
+      self.pos = glm.vec3(0)
+      self.normal = glm.vec3(0)
+      self.material = None
+      self.light = None
