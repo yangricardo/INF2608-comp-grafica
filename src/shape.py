d77c35b2d2e88192f3ef9ee7abe0f50cad04aeee
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:09:29 -0300

    fix: Improve intersection logic in Sphere class for accurate hit detection

--- src/shape.py
+++ src/shape.py
@@ -21,13 +21,22 @@ class Sphere(Shape):
     c = glm.dot(oc, oc) - self.radius**2
     delta = b**2 - 4*a*c
     if delta >= 0:
-        t = (-b - glm.sqrt(delta)) / (2.0 * a)
-        if 0.001 < t < hit.t:
-            hit.t = t
-            hit.pos = ray.o + t * ray.d
-            hit.normal = (hit.pos - self.center) / self.radius
-            hit.material = self.material
-            return True
+      sqrt_d = glm.sqrt(delta)
+      t1 = (-b - sqrt_d) / (2.0 * a)
+      t2 = (-b + sqrt_d) / (2.0 * a)
+
+      t_candidate = None
+      if t1 > 0.001:
+        t_candidate = t1
+      elif t2 > 0.001:
+        t_candidate = t2
+
+      if t_candidate is not None and t_candidate < hit.t:
+        hit.t = t_candidate
+        hit.pos = ray.o + t_candidate * ray.d
+        hit.normal = (hit.pos - self.center) / self.radius
+        hit.material = self.material
+        return True
     return False
 
 
