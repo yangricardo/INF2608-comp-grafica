224547aab4f21367a1b60bf97b94e144fd6a14aa
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:14:30 -0300

    fix: Update Light class constructor to include position parameter

--- src/light.py
+++ src/light.py
@@ -1,10 +1,10 @@
 import glm
 
 class Light:
-  def __init__(self, power: glm.vec3):
+  def __init__(self, pos: glm.vec3, power: glm.vec3):
     self.power = glm.vec3(power)
+    self.pos = pos
 
 class PointLight(Light):
   def __init__(self, pos: glm.vec3, power: glm.vec3):
-    super().__init__(power)
-    self.pos = glm.vec3(pos)
+    super().__init__(pos, power)
