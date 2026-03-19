d9fad77ce989975d38c021217350fb6dc625e42b
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:06:33 -0300

    refactor: Move imports for Material and Light inside TYPE_CHECKING block

--- src/hit.py
+++ src/hit.py
@@ -1,11 +1,14 @@
 import glm
-from typing import Optional
-from material import Material
-from light import Light
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+  from material import Material
+  from light import Light
+
 class Hit:
   def __init__(self, t=float('inf')):
     self.t = t
     self.pos = glm.vec3(0)
     self.normal = glm.vec3(0)
-    self.material: Optional[Material] = None
-    self.light: Optional[Light] = None
+    self.material: Optional['Material'] = None
+    self.light: Optional['Light'] = None
