1c29454e1b06b4eff599abd8d1578ad0383b7b81
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:00:41 -0300

    feat: Add main rendering functionality with camera, scene, and lighting setup

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image
+import glm
+from ray import Ray
+from camera import Camera
+from scene import Scene
+from shape import Sphere
+from material import PhongMaterial
+from light import PointLight
+
+
+def render():
+    W, H = 400, 300
+    cam = Camera((0, 0, 5), (0, 0, 0), (0, 1, 0), 45, W, H)
+    scene = Scene()
+    
+    # Objetos e Luzes
+    mat_red = PhongMaterial((0.1, 0, 0), (0.7, 0, 0), (1, 1, 1), 50)
+    scene.objects.append(Sphere((0, 0, 0), 1.0, mat_red))
+    scene.lights.append(PointLight((5, 5, 5), (150, 150, 150)))
+
+    # Render Loop [cite: 4, 35]
+    img = np.zeros((H, W, 3), dtype=np.uint8)
+    for j in range(H):
+        for i in range(W):
+            xn, yn = (i + 0.5) / W, (j + 0.5) / H
+            ray = cam.generate_ray(xn, yn)
+            color = glm.clamp(scene.trace_ray(ray), 0, 1)
+            img[j, i] = (color * 255)
+
+    Image.fromarray(img).save("render_final.png")
+
+if __name__ == "__main__":
+    render()
