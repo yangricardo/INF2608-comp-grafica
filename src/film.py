4bb7f91eb4e33d725ea826da9ecf617c44f46319
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:12:11 -0300

    fix: Add type hints for parameters in Film class methods

--- src/film.py
+++ src/film.py
@@ -2,9 +2,9 @@ import glm
 import numpy as np
 
 class Film:
-  def __init__(self, width, height):
+  def __init__(self, width: int, height: int):
     self.resolution = (width, height)
     self.image = np.zeros((height, width, 3)) # Buffer de pixels
 
-  def set_pixel(self, i, j, color):
+  def set_pixel(self, i: int, j: int, color: glm.vec3):
     self.image[j, i] = glm.clamp(color, 0, 1)
