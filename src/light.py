421c553c533cc68711fac06a9abd6ceaa15713a4
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-18 21:11:36 -0300

    fix: Add type hints for power and position parameters in Light and PointLight classes

--- src/light.py
+++ src/light.py
@@ -1,10 +1,10 @@
 import glm
 
 class Light:
-  def __init__(self, power):
+  def __init__(self, power: glm.vec3):
     self.power = glm.vec3(power)
 
 class PointLight(Light):
-  def __init__(self, pos, power):
+  def __init__(self, pos: glm.vec3, power: glm.vec3):
     super().__init__(power)
     self.pos = glm.vec3(pos)
