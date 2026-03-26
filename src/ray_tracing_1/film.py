a002c14c9b60d787f4152a3e93c1ba0fe9ac6b48
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-25 22:59:02 -0300

    feat: Enhance Film class with width and height attributes for pixel sampling

--- src/ray_tracing_1/film.py
+++ src/ray_tracing_1/film.py
@@ -5,8 +5,14 @@ import numpy as np
 
 class Film:
   def __init__(self, width: int, height: int):
+    self.width = width
+    self.height = height
     self.resolution = (width, height)
     self.image = np.zeros((height, width, 3)) # Buffer de pixels
 
   def set_pixel(self, i: int, j: int, color: glm.vec3):
     self.image[j, i] = glm.clamp(color, 0, 1)
+
+  def get_sample(self, i, j):
+    """Retorna as coordenadas normalizadas (0 a 1) para o pixel (i, j)"""
+    return (i + 0.5) / self.width, (j + 0.5) / self.height
