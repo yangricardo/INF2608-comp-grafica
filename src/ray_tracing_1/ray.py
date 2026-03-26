2ef54cb3f70d1fcfee780de220304fb091d6e77d
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-26 00:16:42 -0300

    fix: Remove normalization from direction vector in Ray class constructor

--- src/ray_tracing_1/ray.py
+++ src/ray_tracing_1/ray.py
@@ -5,4 +5,4 @@ import glm
 class Ray:
   def __init__(self, origin: glm.vec3, direction: glm.vec3):
     self.o = glm.vec3(origin)
-    self.d = glm.normalize(glm.vec3(direction))
+    self.d = glm.vec3(direction)
