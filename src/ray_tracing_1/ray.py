6618fa8a820372c9cb17d10cc490f2a693e8fff6
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-03-26 00:46:00 -0300

    fix: Add comment in Ray class constructor for clarity on origin and direction storage

--- src/ray_tracing_1/ray.py
+++ src/ray_tracing_1/ray.py
@@ -4,5 +4,6 @@ import glm
 
 class Ray:
   def __init__(self, origin: glm.vec3, direction: glm.vec3):
+    # Slide 4, p. 5-6 e p. 25-29: o raio armazena origem e direção para ser testado contra a cena.
     self.o = glm.vec3(origin)
     self.d = glm.vec3(direction)
