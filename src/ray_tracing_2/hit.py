69c139e3402501e4c35181e1e2c0a6570382c616
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2026-04-12 19:23:20 -0300

    feat: add ray import and reorganize Hit class attributes for clarity

--- src/ray_tracing_2/hit.py
+++ src/ray_tracing_2/hit.py
@@ -6,6 +6,7 @@ from typing import Optional, TYPE_CHECKING
 if TYPE_CHECKING:
   from ray_tracing_2.material import Material
   from ray_tracing_2.light import Light
+  from ray_tracing_2.ray import Ray
 
 class Hit:
   def __init__(self, t: float = float('inf')):
@@ -13,6 +14,15 @@ class Hit:
     self.t = t
     self.pos = glm.vec3(0)
     self.normal = glm.vec3(0)
+    self.geo_normal = glm.vec3(0)
     self.material: Optional['Material'] = None
     self.light: Optional['Light'] = None
+    self.front_face: bool = True
     self.backfacing: bool = False
+
+  def set_face_normal(self, ray: 'Ray', outward_normal: glm.vec3):
+    outward = glm.normalize(glm.vec3(outward_normal))
+    self.geo_normal = outward
+    self.front_face = glm.dot(ray.d, outward) < 0.0
+    self.backfacing = not self.front_face
+    self.normal = outward if self.front_face else -outward
